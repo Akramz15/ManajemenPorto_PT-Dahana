0b0833e0e3c66183fb2133e4f6b08f4d7911b5fa
Akram <naufalakramziyad53@gmail.com>
2026-07-27 14:22:06 +0700

    style: visually center logo by nudging to the right to compensate for asymmetric graphic weight

--- frontend/src/pages/UpdatePasswordPage.tsx
+++ frontend/src/pages/UpdatePasswordPage.tsx
@@ -79,7 +79,7 @@ export default function UpdatePasswordPage() {
             <img
               src={logoDahana}
               alt="PT Dahana BizPort"
-              className="h-16 w-auto object-contain drop-shadow-sm"
+              className="h-16 w-auto object-contain drop-shadow-sm translate-x-3"
             />
           </div>
 
